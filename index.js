9dda46df84b57754abd9d6ea528b8f51e1b1d09f
simmy-ts <simmy.gupta@tabsquare.com>
2023-12-27 15:13:00 +0530

    Boiler plate for react with parcel

---
+++ index.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+    return <div>In React</div>
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App/>)
